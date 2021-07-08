811ffd829c2268cee5edfa39e0c88a6a413abeb7
Gouthami R <69711081+Gouthami492@users.noreply.github.com>
2021-07-08 14:54:16 +0530

    Create game.h

---
+++ game.h
@@ -0,0 +1,11 @@
+#define LEFT -2
+#define UP 1
+#define DOWN -1
+#define RIGHT 2
+#define MAX 60
+void initGrid(int,int);
+void drawGrid();
+void unit(int x, int y);
+void drawsnake();
+void random(int& x, int& y);
+void drawfood();
